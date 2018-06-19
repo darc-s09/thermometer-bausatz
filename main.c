7ee726393601b08cf2228d25b9cc87c6dc191d43
Joerg Wunsch <dl8dtl@s09.sax.de>
2018-06-19 23:44:22 +0200

    First version with working Charliexplexing.

---
+++ main.c
@@ -0,0 +1,184 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+#define NLEDS 6 // per group
+#define NDIM  5 // duty cycle for dimmed LEDs
+
+                // Duty cycle 1:10 would result in better contrast
+                // between DIM and ON LEDs, but requires higher
+                // heartbeat clock to avoid flicker.
+
+// With 2000 Hz heartbeat clock, charlieplexing frequency is 333 Hz,
+// and dimmed LED flicker frequency is 66 Hz.
+#define F_TIMER 2000
+
+enum ledstatus
+{
+    OFF, DIM, ON
+};
+
+enum ledstatus leds[2 * NLEDS];
+
+static volatile uint32_t seconds;
+
+/*
+ * LEDs are operated in "Charliexplexing" mode
+ * https://en.wikipedia.org/wiki/Charlieplexing
+ *
+ * This requires to re-decide about each LED pin's state between high,
+ * low, and off (high-Z) on each timestep.  On the pro side, it allows
+ * for connecting a fairly large number of LEDs with only few
+ * controller pins.  Very large matrices however cause a poor LED duty
+ * cycle, thus we operate two groups of 6 LEDs each on 3 controller
+ * pins per group, yielding a 1:6 duty cycle for each LED.  Depending
+ * on battery voltage, this yields in an effective LED current of
+ * about 30 mA / 6 = 5 mA for each active LED.
+ *
+ * Note that LED12 is not mounted, as using an odd number of LEDs
+ * makes the overall design look nicer, since there's a single center
+ * LED.
+ */
+static void
+led_update(void)
+{
+    static uint8_t cycle;
+    static uint8_t dim_cycle;
+
+    if (++cycle == NLEDS)
+    {
+        cycle = 0;
+        // LEDs in DIM state are only activated each NDIMth cycle
+        // LEDs in ON state are activated each time their cycle
+        // number matches
+        if (++dim_cycle == NDIM)
+            dim_cycle = 0;
+    }
+
+    // LEDs are on PA0, PA1, PA2
+    uint8_t port = PORTA & ~0b00000111; // preserve all non-LED bits,
+    uint8_t ddr  = DDRA  & ~0b00000111; // but mask LED bits to 0
+
+    if (leds[cycle] == OFF ||
+        (leds[cycle] == DIM && dim_cycle != 0))
+    {
+        ddr |= 0b00000111; // output low, all off
+    }
+    else // this LED needs to be turned on during this cycle
+    {
+        /*
+         * charlieplexing matrix lower LEDs
+         *
+         * LED1: PA0 high, PA1 low,  PA2 off
+         * LED2: PA0 low,  PA1 high, PA2 off
+         * LED3: PA0 off,  PA1 low,  PA2 high
+         * LED4: PA0 off,  PA1 high, PA2 low
+         * LED5: PA0 high, PA1 off,  PA2 low
+         * LED6: PA0 low,  PA1 off,  PA2 high
+         * off:  PA0 low,  PA1 low,  PA2 low
+         */
+
+        if (cycle == 0)      { ddr |= 0b00000011; port |= 0b00000001; }
+        else if (cycle == 1) { ddr |= 0b00000011; port |= 0b00000010; }
+        else if (cycle == 2) { ddr |= 0b00000110; port |= 0b00000100; }
+        else if (cycle == 3) { ddr |= 0b00000110; port |= 0b00000010; }
+        else if (cycle == 4) { ddr |= 0b00000101; port |= 0b00000001; }
+        else                 { ddr |= 0b00000101; port |= 0b00000100; }
+    }
+
+    PORTA = port;
+    DDRA = ddr;
+
+    // LEDs are on PB0, PB1, PB2
+    port = PORTB & ~0b00000111;
+    ddr = DDRB & ~0b00000111;
+
+    if (leds[cycle + NLEDS] == OFF ||
+        (leds[cycle + NLEDS] == DIM && dim_cycle != 0))
+    {
+        ddr |= 0b00000111; // output low, all off
+    }
+    else
+    {
+        /*
+         * charlieplexing matrix upper LEDs
+         *
+         * LED7:  PB0 high, PB1 off,  PB2 low
+         * LED8:  PB0 low,  PB1 off,  PB2 high
+         * LED9:  PB0 off,  PB1 high, PB2 low
+         * LED10: PB0 off,  PB1 low,  PB2 high
+         * LED11: PB0 high, PB1 low,  PB2 off
+         * LED12: PB0 low,  PB1 high, PB2 off  // not mounted
+         * off:   PB0 low,  PB1 low,  PB2 low
+         */
+        if (cycle == 0)      { ddr |= 0b00000101; port |= 0b00000001; }
+        else if (cycle == 1) { ddr |= 0b00000101; port |= 0b00000100; }
+        else if (cycle == 2) { ddr |= 0b00000110; port |= 0b00000010; }
+        else if (cycle == 3) { ddr |= 0b00000110; port |= 0b00000100; }
+        else if (cycle == 4) { ddr |= 0b00000011; port |= 0b00000001; }
+        //else               { ddr |= 0b00000011; port |= 0b00000010; }
+    }
+
+    PORTB = port;
+    DDRB = ddr;
+}
+
+
+ISR(TIM1_COMPA_vect)
+{
+    static int ticks;
+
+    if (++ticks == F_TIMER)
+    {
+        ticks = 0;
+        seconds++;
+    }
+
+    led_update();
+}
+
+static void
+setup(void)
+{
+    /* TIMER1: interrupt each 1/F_TIMER */
+    OCR1A = F_CPU / F_TIMER;
+    TIMSK1 = _BV(OCIE1A);
+    TCCR1B = _BV(WGM12) | _BV(CS10); /* CTC mode, full clock */
+
+    /* PA3: jumper 2 */
+    /* PA4/5/6: ISP / USI */
+    /* PA7: jumper 1 */
+    PORTA = _BV(3) | _BV(4) | _BV(5) | _BV(6) | _BV(7);
+
+    /* LED group 1: PA0/1/2 */
+    /* LED group 2: PB0/1/2 */
+
+    sei();
+}
+
+static void
+loop(void)
+{
+    sleep_mode();
+}
+
+
+int main(void) __attribute__((OS_main));
+int
+main(void)
+{
+    setup();
+
+    leds[3] = DIM;
+    leds[8] = ON;
+
+    for (;;)
+      loop();
+}
+
+/*
+ * Local Variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
