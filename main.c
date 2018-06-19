397e16a8b8a94a7928e1f9185dacf9b5b69b2f2b
Joerg Wunsch <dl8dtl@s09.sax.de>
2018-06-19 23:45:35 +0200

    add copyright

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <dl8dtl@s09.sax.de> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.        Joerg Wunsch
+ * ----------------------------------------------------------------------------
+ *
+ */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
